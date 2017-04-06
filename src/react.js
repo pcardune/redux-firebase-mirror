f1a4f46e24f92c5e8d83e2a1800676939f991e62
Paul Carduner <paul@carduner.net>
2017-04-06 12:29:18 -0700

    add subscribeProps function

--- src/react.js
+++ src/react.js
@@ -2,6 +2,9 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import type {Dispatch} from 'redux';
+import {compose} from 'redux';
+
+import {subscribeToValues} from './actions';
 
 export function subscribePaths<RS, NP:Object, D, P:Object, S, C: React$Component<D, P, S>>(
   mapPropsToPaths: (state: RS, props: NP) => string[]
@@ -18,7 +21,7 @@ export function subscribePaths<RS, NP:Object, D, P:Object, S, C: React$Component
       };
 
       componentDidMount() {
-        this.props.dispatch(actions.subscribeToValues(mapPropsToPaths(this.props.state, this.props)));
+        this.props.dispatch(subscribeToValues(mapPropsToPaths(this.props.state, this.props)));
       }
 
       componentDidUpdate() {
@@ -29,7 +32,7 @@ export function subscribePaths<RS, NP:Object, D, P:Object, S, C: React$Component
         // so to play it safe, ditch this micro-opitmization for now.
         // const prevPaths = new Set(mapPropsToPaths(this.props.state, prevProps));
         // const newPaths = paths.filter(path => !prevPaths.has(path));
-        this.props.dispatch(actions.subscribeToValues(paths));
+        this.props.dispatch(subscribeToValues(paths));
       }
 
       render() {
@@ -41,5 +44,37 @@ export function subscribePaths<RS, NP:Object, D, P:Object, S, C: React$Component
         return <ComponentToWrap {...rest} />;
       }
     });
+  };
+}
+
+export function subscribeProps(mapPropsToSubscriptionsMaybeFunc) {
+  let mapPropsToSubscriptions;
+  if (typeof mapPropsToSubscriptionsMaybeFunc == 'function') {
+    mapPropsToSubscriptions = mapPropsToSubscriptionsMaybeFunc;
+  } else {
+    mapPropsToSubscriptions = () => mapPropsToSubscriptionsMaybeFunc;
+  }
+  return (ComponentToWrap) => {
+    return compose(
+      subscribePaths(
+        (state, ownProps) => {
+          const subscriptionsMap = mapPropsToSubscriptions(state, ownProps);
+          return Object.values(subscriptionsMap).reduce(
+            (paths, subscription) => paths.concat(subscription.paths(state, ownProps)),
+            []
+          );
+        }
+      ),
+      connect(
+        (state, ownProps) => {
+          const subscriptionsMap = mapPropsToSubscriptions(state, ownProps);
+          const props = {};
+          for (const key in subscriptionsMap) {
+            props[key] = subscriptionsMap[key].value(state, ownProps);
+          }
+          return props;
+        }
+      ),
+    )(ComponentToWrap);
   };
 }
