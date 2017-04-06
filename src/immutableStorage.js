44555e4f97b919cb69c4ece90247c9dbd2821c0b
Paul Carduner <paul@carduner.net>
2017-04-06 12:28:44 -0700

    Allow leading and trailing slashes in selector functions

--- src/immutableStorage.js
+++ src/immutableStorage.js
@@ -1,13 +1,14 @@
 //@flow
 import * as Immutable from 'immutable';
 import type {JSONType, StorageAPI} from './types';
+import {splitPath} from './util';
 
 type MirrorType = Immutable.Map<string, mixed>;
 type ValueType = mixed;
 
 const immutableStorage: StorageAPI<MirrorType, ValueType> = {
   getKeysAtPath(mirror: MirrorType, path: string): string[] {
-    const map = mirror.getIn(path.split('/'), Immutable.Map());
+    const map = mirror.getIn(splitPath(path), Immutable.Map());
     if (!(map instanceof Immutable.Map)) {
       return [];
     }
@@ -15,13 +16,13 @@ const immutableStorage: StorageAPI<MirrorType, ValueType> = {
   },
 
   getValueAtPath(mirror: MirrorType, path: string): ValueType {
-    return mirror.getIn(path.split('/'));
+    return mirror.getIn(splitPath(path));
   },
 
   setValues(mirror: MirrorType, values: {[path: string]: JSONType}): MirrorType {
     return mirror.withMutations((mirror) => {
       Object.entries(values).forEach(([path, value]) => {
-        mirror.setIn(path.split('/'), Immutable.fromJS(value));
+        mirror.setIn(splitPath(path), Immutable.fromJS(value));
       });
       return mirror;
     });
---
+++ src/util.js
@@ -0,0 +1,13 @@
+export function splitPath(path) {
+  if (path[0] === '/') {
+    // remove leading slash
+    path = path.slice(1);
+  }
+
+  if (path[path.length - 1] === '/') {
+    // remove trailing slash
+    path = path.slice(0, path.length - 1)
+  }
+
+  return path.split('/');
+}
